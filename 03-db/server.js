7661343ead1ac49b9df3df71f2aac20fc0707fae
Piyush <197909016+Piyush-6177@users.noreply.github.com>
2026-08-11 22:14:13 +0530

    learnt database integration with mongoDB and schema creation with a proper folder structure

---
+++ 03-db/server.js
@@ -0,0 +1,12 @@
+// server.js >> to start the server
+require('dotenv').config()
+const app = require('./src/app')
+
+const connectDB = require("./src/db/db")
+
+connectDB()
+
+app.listen(3000, ()=>{
+    console.log("The server is running on port : 3000");
+    
+})
---
+++ 03-db/src/app.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const noteModel = require("./models/note.model")
+
+
+const app = express()   // server instance 
+app.use(express.json()) // middleware 
+
+// notes = {title,description}
+
+app.post("/notes",async (req,res)=>{
+
+    const data = req.body
+
+    await noteModel.create({
+        title : data.title,
+        description : data.description
+    })
+
+    res.status(201).json({
+        message : "Note created"
+    })
+})
+
+app.get("/notes",async(req,res)=>{
+
+    const notes = await noteModel.find() // [] 
+    // const notes = await noteModel.findOne({
+    //     title : "The Theory of Everything"
+    // })
+
+    // find >> [{},{},..] or []
+    // findOne >> {} or null 
+
+    res.status(201).json({
+        message : "notes fetched successfully",
+        notes : notes
+    })
+})
+
+app.delete("/notes/:id", async (req,res)=>{
+
+    const id = req.params.id
+
+    await noteModel.findOneAndDelete({
+        _id : id
+    })
+
+    res.status(200).json({
+        message : "note deleted successfully"
+    })
+})
+
+app.patch("/notes/:id",async (req,res)=>{
+
+    const id = req.params.id
+    const description = req.body.description
+
+    await noteModel.findOneAndUpdate({_id:id}, {
+        description : description
+    })
+
+    res.status(200).json({
+        message : "note updated successfully"
+    })
+})
+
+
+module.exports = app
